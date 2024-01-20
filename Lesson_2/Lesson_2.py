f1dbc6f1017c57446f6f7e911cc4365cd8cb63ea
vinch <djkif1212@gmail.com>
2024-01-20 23:26:38 +0200

    less_2

---
+++ Lesson_2/Lesson_2.py
@@ -0,0 +1,45 @@
+# todo конкатенація може буті додавання (стр +стр) і може бути множеня (стр * інт)
+
+# var = "5"
+# var_1 =12
+# print(int(var)* var_1)# 60
+# print(var * var_1) # 555555555555
+# print(str(var_1) +var) # 125
+
+
+# var_a = "new"
+# var_b = "student"
+# print(var_a, var_b)# new student
+# print(var_a+var_b)# newstudent
+
+
+
+# first_1 =20
+# first_2 = 10
+# print(first_1)
+# print(first_2)
+
+##todo Синтаксичний цукор
+# first_1, first_2 = first_2, first_1
+# print(first_1)
+
+
+# # множинне присвоєння
+# a,b,c = 10,12,15
+# print(a,b,c)
+#
+# #степінь
+# print(2**3)
+#
+# #квадратний корінь
+# print(25**0.5)
+
+# todo (Built-in) - это функции встроенные в интерпретатор Python
+#   и для их использования в программах не надо импортировать модули
+
+#Новий тип данних bool. Пишеться з великої літери True, False
+
+
+
+
+
