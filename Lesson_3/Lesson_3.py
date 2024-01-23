ae7b22ff7744be7a83aaff7262e763f3ba9d70e3
Vinch <djkif1212@gmail.com>
2024-01-23 09:14:10 +0200

    lesson 3

---
+++ Lesson_3/Lesson_3.py
@@ -0,0 +1,58 @@
+# int_num =100
+# if int_num > 10:
+#     print(">10")
+# elif int_num > 100:
+#     print(">100")
+
+# todo Розбір на просту структуру
+
+# print(True or True)#True
+# print(True or False)#True
+# print(False or False)#False
+# print(True and False)#False
+# print(False and False)#False
+# print(True and 1)# 1
+# print(True and bool(1))
+
+
+# todo round() - Округляє
+
+# print(0.0001 + 0.0002)# глюк pythonA
+# print((1+2)/10000)# перший варіант рішення
+# #import decimal - імпорт нового типу данних який працює без помилок
+# a = 0.0001+0.0002
+# print(a)
+# a=round(a,5)
+# print(a)
+
+# todo in str
+
+str_a = "knkl Klkj nnjfhgfhgf"
+print("jfh" in str_a)  # True #перевірка чи входе текст в стрічку, чутливий до регістру
+
+#todo: f string
+name = "Ivan"
+age = 32
+print(f"Hi {name}, you are {age} old")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
