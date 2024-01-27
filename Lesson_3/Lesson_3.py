db17edfd1bf4bd786dc8a087b5fd80b8fa9bec4b
vinch <djkif1212@gmail.com>
2024-01-27 16:14:09 +0200

    lesson 3

--- Lesson_3/Lesson_3.py
+++ Lesson_3/Lesson_3.py
@@ -4,11 +4,6 @@
 #     print("!!!")
 
 
-
-
-
-
-
 # int_num =100
 # if int_num > 10:
 #     print(">10")
@@ -38,57 +33,91 @@
 
 # todo in str
 
-#str_a = "knkl Klkj nnjfhgfhgf"
-#print("jfh" in str_a)  # True #перевірка чи входе текст в стрічку, чутливий до регістру
+# str_a = "knkl Klkj nnjfhgfhgf"
+# print("jfh" in str_a)  # True #перевірка чи входе текст в стрічку, чутливий до регістру
 
-#todo: f string
+# todo: f string
 
 # name = "Ivan"
 # age = 32
 # print(f"Hi {name}, you are {age} old")###
 
-#todo:len() повертає кількість елементів
-#print(len("any text"))# 8 elements
-
+# todo:len() повертає кількість елементів
+# print(len("any text"))# 8 elements
 
 
 # todo: strip(), lstrip(), rstrip()
 
-#str= "+++asd+++asd+++"
+# str= "+++asd+++asd+++"
 
 # print(str.strip("+"))# = asd+++asd видаляє з 2х сторін
 # print(str.rstrip("+"))# = +++asd+++asd видаляє зправа
 # print(str.lstrip("+"))# = asd+++asd+++ видаляє зліва
 
-#todo: .count(), .isdigit(), .find(), .isalpha()
+# todo: .count(), .isdigit(), .find(), .isalpha()
 
-str= "+++asd+++asd+++"
-print(str.count("+")) # показує скілки обраних елементів в змінній/об'єкті
+# str_1 = "+++asd+++asd+++"
+# print(str_1.count("+")) # показує скілки обраних елементів в змінній/об'єкті
 
-print(str.isdigit()) #
-print(str.find())
-print(str.isalpha())
-
-
-
-
+# str_1= "999"
+# str_2= "str"
+# print(str_1.isdigit()) # використовуеться тільки з стр, перевіряє щоб строка була тільки з цифр
+# print(str_2.isalpha()) # використовуеться тільки з стр, перевіряє щоб строка була тільки з літер
+# print(str.find())
 
 
+# num_1 = "999"
+# int_2 = "str"
+# print(num_1.isdigit())
+# print(int_2.isdigit())
+#
+# var_1= input()
+# var_2= input()
+#
+# if var_1.isdigit() and var_2.isdigit():
+#     print((int(var_1)+int(var_2))*3)
+# else:
+#     print("Мипрацюємо тільки з цілими числами")
+
+# todo: Зрізи в str
+
+# string_a= "123456789012345678901"
+# var= 10
+# print(string_a[0])# звертаємось до першого елементу
+# print(string_a[3])# звертаємось до четвертого елементу
+# print(string_a[0:8])# виводимо з 1го по 7й елемент, останній елемент не показує
+# print(string_a[2:var])# виводимо з 2го по вказаний в змінній
+# print(string_a[:8])# виводимо з 1го по вказаний
+# print(string_a[3:])# виводимо з вказаного по останній
+# print()
+# print(string_a[-1:])# виводимо останній елемент
+# print(string_a[-5:-3])# виводимо з -5го по -3й елементи
+# print(string_a[::2])# 3й параметр вказує на крок виводу - тут буде показувати кожен 2й символ
+# print(string_a[len(string_a)//2+1:])# вираховуемо середину , додаємо +1 символ і виводимо
+
+# todo: приоритети операцій
+# print(1 + 2 * 3)
+# print((1 + 2) * 3)
+# () - Дужки
+# ** - Піднесення до степеня
+# +x, -x, ~x - Унарні плюс, мінус і бітове заперечення
+# *, /, //, %
+# *, /, //, % - Множення, ділення, цілочисельне ділення, остача від ділення
+# +, - - Додавання та віднімання
+# <<, >> - Бітові зсуви
+# & - Бітове І (AND)
+# ^ - Бітове виключне АБО (XOR)
+# | - Бітове АБО (OR)
+# ==, !=, >, >=, <, <=, is, is not, in, not in - Порівняння, перевірка ідентичності, перевірка належності
+# not - Логічне НЕ (NOT)
+# and - Логічне І (AND)
+# or - Логічне АБО (OR)
+
+
+# todo: list
 
 
 
 
 
-#.split("/")
-
-
-
-
-
-
-
-
-
-
-
-
+# .split("/")
