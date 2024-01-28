bb2c7730d5116b58a486f43ec94ee32538cd11c4
vinch <djkif1212@gmail.com>
2024-01-28 18:52:29 +0200

    lesson 19

--- Lesson_19/conftest.py
+++ Lesson_19/conftest.py
@@ -1,8 +1,18 @@
+# import pytest
+# from selenium import webdriver
+#
+# @pytest.fixture
+# def chrome():
+#     driver = webdriver.Chrome(executable_path="chromedriver")
+#     yield driver
+#     driver.quit()
+
+
 import pytest
 from selenium import webdriver
 
 @pytest.fixture
 def chrome():
-    driver = webdriver.Chrome(executable_path="chromedriver")
+    driver = webdriver.Chrome(executable_path="Lesson_19/chromedriver")
     yield driver
     driver.quit()
---
+++ Lesson_19/tests/tests_elements.py
@@ -0,0 +1,33 @@
+from selenium.webdriver.remote.webelement import WebElement
+
+from Lesson_19.DinamicPropertiesPage import PageDynamicProperties
+from Lesson_19.ElementsPage import ElementsPage
+
+class TestElementsPage:
+    def test_page(self, chrome):
+        page = ElementsPage(chrome)
+        page.open()
+        elements = page.get_elements_page_categories()
+        assert len(elements) == 33
+
+    # todo: перевірити відповіді  всіх 33х елементів в elements
+    # assert elements [2] == "Radio Buttons"
+
+    def test_is_button_enabled(self,chrome):
+        page = PageDynamicProperties(chrome)
+        page = open()
+        button: WebElement = page.disable_enable_button()
+        button.click()
+
+    def test_isbutton_show(self,chrome):
+        page= PageDynamicProperties(chrome).open()# короткий запис
+        button: WebElement = page.button_invisible_visible()
+        button.click()
+
+
+
+
+
+
+
+
