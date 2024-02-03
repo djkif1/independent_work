8f52be15cafbe8d0a18884ce6ffd9fec3500585d
vinch <djkif1212@gmail.com>
2024-02-03 13:23:37 +0200

    lesson 19

---
+++ Lesson_19/Home_Work_19/tests/tests_elements.py
@@ -0,0 +1,31 @@
+from Lesson_19.DinamicPropertiesPage import PageDynamicProperties
+from selenium.webdriver.remote.webelement import WebElement
+from Lesson_19.ElementsPage import ElementsPage
+
+class TestElementsPage:
+    def test_page(self, chrome):
+        page = ElementsPage(chrome)
+        page.open()
+        elements = page.get_elements_page_categories()
+        assert len(elements) == 33
+
+    #  todo перевірити відповіді всіх 33 елементів в елементс
+    #  assert elements[2] == "Radio Button"
+
+    def test_is_button_enabled(self, chrome):
+        page = PageDynamicProperties(chrome)# Довгий запис
+        page.open()
+        button: WebElement = page.disable_enable_button()
+        button.click()
+        pass
+    def test_is_button_shown(self, chrome):
+        page = PageDynamicProperties(chrome).open()  # короткий запис
+        button: WebElement = page.button_invisible_visible()
+        button.click()
+
+
+
+
+
+
+
