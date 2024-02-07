33d368df0f04b2e21f00ffa69bbdd11148b0bfa5
vinch <djkif1212@gmail.com>
2024-02-07 21:29:05 +0200

    Lesson 4

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
+# version1 = '123.23.23.2a'
+# version2 = '123.23.23.3'
+# def chek_ver(version1, version2):
+#     version1.strip('.')
+#     version2.strip('.')
+#     if version1 > version2:
+#         print(version1)
+#     else:
+#         print(version2)
+#
+# chek_ver(version1,version2)
 
 
+version1 = '123.23.23.2a'
+version2 = '123.23.23.3'
+def uu21(v1,v2):
+    print (v1>v2)
 
 
-
-
-
-
-
-
+uu21(version1,version2)
 
