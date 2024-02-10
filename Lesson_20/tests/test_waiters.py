88e7e0623cc8f739fc93bf0e0f5a6a9bfc2b1ff8
vinch <djkif1212@gmail.com>
2024-02-10 10:10:43 +0200

    Home Work 20

--- Lesson_20/tests/test_waiters.py
+++ Lesson_20/tests/test_waiters.py
@@ -20,16 +20,17 @@ class TestWaiters:
         self.driver.get("https://demoqa.com/dynamic-properties")
         colored_button_loc = (By.ID, "colorChange")
         colored_button = self.driver.find_element(*colored_button_loc)
-        WebDriverWait(self.driver, 5).until(ec.text_to_be_present_in_element_attribute(colored_button_loc,
+        WebDriverWait(self.driver, 6).until(ec.text_to_be_present_in_element_attribute(colored_button_loc,
                                                                                       "class", "text-danger"))
         colored_button.click()
-        assert colored_button.text == "Color Change"
+        assert "mt-4 text-danger btn btn-primary" in colored_button.get_attribute("class")
+
 
 
     def test_connection_enable(self):
         self.driver.get("https://demoqa.com/dynamic-properties")
         enable_button_loc = (By.XPATH, '//*[@id="enableAfter"]')
-        WebDriverWait(self.driver, timeout=5).until(ec.element_to_be_clickable(enable_button_loc))
+        WebDriverWait(self.driver, timeout=6).until(ec.element_to_be_clickable(enable_button_loc))
         enable_button = self.driver.find_element(*enable_button_loc)
         enable_button.click()
         assert enable_button.is_enabled(), "Кнопка не в стані enabled"
